881842bfdaf8842284f22d5b86ccc05b3e32b673
Mansi24k <mansikrishnapurkar@gmail.com>
2023-12-25 18:32:58 +0530

    variables in java

---
+++ Variable.java
@@ -0,0 +1,11 @@
+//code to define variable
+public class Variable {
+    public static void main (String args[]){
+        int a = 15;                 // variable a stores integer value
+        float b = 15.6f;            // variable b stored float value
+        String c = "Hello World!";  // Variable c stores  string value
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
